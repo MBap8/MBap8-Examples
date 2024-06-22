f0de7c98e09d6d1d7d7ca06086dca5f47ee83ebf
Mark Baptista <m.baptista@outlook.com>
2024-06-22 11:46:08 +1000

    Update Codility_Example_1.java

--- Codility_Example_1/Codility_Example_1.java
+++ Codility_Example_1/Codility_Example_1.java
@@ -3,12 +3,14 @@ class Untitled {
 		Solution sol = new Solution();
 		int[] levels = {5,8,3,9,10,12,53};
 		int diff = 3;
-		int res = sol.days(levels, diff);
+		int res = sol.newDays(levels, diff);
 		System.out.println(res);
+		System.out.println(sol.days(levels, diff));
 	}
 }
 
 class Solution {
+	// this solution is wrong
 	public int days(int[] levels, int diff){
 		int days = 0;
 		for (int i = 0; i < levels.length;){
@@ -24,6 +26,34 @@ class Solution {
 				days++;
 				i++;
 			}
+		}
+		return days;
+	}
+	
+	// new solution
+	public int newDays(int[] levels, int diff){
+		if (levels.length == 1){
+			return 1;
+		}
+		int days = 0;
+		int minDiff = levels[0];
+		int maxDiff = levels[0];
+		for (int i = 1; i < levels.length; i++){
+			if (minDiff > levels[i]){
+				minDiff = levels[i];
+			} else if (maxDiff <= levels[i]){
+				maxDiff = levels[i];
+			}
+			
+			if (maxDiff - minDiff > diff){
+				days++;
+				minDiff = levels[i];
+				maxDiff = levels[i];
+				if (i + 1 >= levels.length){
+					days++;
+				}
+			}
+			
 		}
 		return days;
 	}
